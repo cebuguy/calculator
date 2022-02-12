3bb23c56fe570c8e48db273c5503bf2870f3eea9
trungn <tknghiem1@gmail.com>
2022-02-11 17:51:11 -0800

    add button hover functionality

---
+++ script.js
@@ -0,0 +1,38 @@
+const add = function(num1,num2) {
+    return num1 + num2;
+}
+
+const subtract = function(num1,num2) {
+    return num1 - num2;
+}
+
+const multiply = function(num1,num2) {
+    return num1 * num2;
+}
+
+const divide = function(num1,num2) {
+    return num1/num2;
+}
+
+const operate = function(num1,num2) {
+    
+}
+
+//declare DOM keys
+const key1 = document.querySelector('.key1');
+const key2 = document.querySelector('.key2');
+const key3 = document.querySelector('.key3');
+const key4 = document.querySelector('.key4');
+const key5 = document.querySelector('.key5');
+const key6 = document.querySelector('.key6');
+const key7 = document.querySelector('.key7');
+const key8 = document.querySelector('.key8');
+const key9 = document.querySelector('.key9');
+const key0 = document.querySelector('.key0');
+const keyPlus = document.querySelector('.key-plus');
+const keyMinus = document.querySelector('.key-minus');
+const keyMultiply = document.querySelector('.key-multiply');
+const keyDivide = document.querySelector('.key-divide');
+const keyEqual = document.querySelector('.key-equal');
+const keyDecimal = document.querySelector('.key-decimal');
+
